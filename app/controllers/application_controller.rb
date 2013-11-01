d86dbdf8d7945177430eec73b60362ba74eec16d
Felipe Conti <felipeconti18@gmail.com>
2013-11-01 16:54:13 -0200

    Initial Commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+	# Prevent CSRF attacks by raising an exception.
+	# For APIs, you may want to use :null_session instead.
+	protect_from_forgery with: :exception
+
+	before_filter :authenticate_user!, :load_users
+
+	private
+
+	def load_users
+		@users = User.all
+	end
+end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sitio::Application.config.session_store :cookie_store, key: '_Sitio_session'
