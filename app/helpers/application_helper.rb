c508247e17ad9a8485bfe84cd5a7348bb7d3f029
Felipe Conti <felipeconti18@gmail.com>
2014-02-01 17:41:00 +0000

    Small fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,8 @@ module ApplicationHelper
 
   def user_can_modify?(event)
   	# require "pry"; binding.pry
-    unless current_user.super_user or 
-           (event and current_user.holders.find_by_event_id(event) and
-            current_user.holders.find_by_event_id(event).edit)
-      false
-    else
-  	  true
-    end
+    current_user.super_user or (event and 
+                                current_user.holders.find_by_event_id(event) and 
+                                current_user.holders.find_by_event_id(event).edit)
   end
 end
